ad7646c9592862e6121262d3c68ecf011cb7051a
Pablo Uceda Fuentes <pucefue@alu.upo.es>
2023-12-29 18:52:29 +0100

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,12 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Desarrollo Web",
+  "Inclusión Digital",
+  "Ciberseguridad",
+  "Microsoft 365",
+  "Interfaces Web",
+  "Diseño Gráfico",
 ];
 
 /**
